ffed2775470c82d377f5ac2bc31e86e5890f5b1d
Christian Blaschke <mail@platdesign.de>
2018-01-11 17:45:28 +0100

    tested example

--- examples/basic/basic.cpp
+++ examples/basic/basic.cpp
@@ -1,13 +1,24 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 #include <Binair.h>
 
+#define WLAN_SSID "yourSSID"
+#define WLAN_PASS "yourPass"
 
 
 void setup() {
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection error");
+  }
+
   BINAIR("[vendor]", "[product]", "[token]", "[platform]", "[version]");
 }
 
-
 void loop() {
-	Binair.check();
+  Binair.check();
 }
